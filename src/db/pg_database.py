546a2e07d5a2131e623025a2f29e3a06e7a82f97
RozumDoKapsy <sulda.str@gmail.com>
2025-04-29 19:24:16 +0200

    fix: change table_name -> db_name

--- src/db/pg_database.py
+++ src/db/pg_database.py
@@ -12,23 +12,23 @@ class PostgreSaver:
         self.creds = creds
         self.logger = logging.getLogger(self.__class__.__name__)
 
-    def _create_session(self, table_name: str):
-        db_url = f"postgresql://{self.creds['username']}:{self.creds['password']}@{self.creds['hostname']}:5432/{table_name}"
+    def _create_session(self, db_name: str):
+        db_url = f"postgresql://{self.creds['username']}:{self.creds['password']}@{self.creds['hostname']}:5432/{db_name}"
         engine = create_engine(db_url)
         Session = sessionmaker(bind=engine)
         session = Session()
         return session
 
-    def save_bulk(self, table_name: str, records: List[Union[SatelliteImageMetadata, WeatherHourly]]):
-        session = self._create_session(table_name)
+    def save_bulk(self, db_name: str, records: List[Union[SatelliteImageMetadata, WeatherHourly]]):
+        session = self._create_session(db_name)
 
         session.add_all(records)
         session.commit()
-        self.logger.info(f'{len(records)} records saved to: {table_name}')
+        self.logger.info(f'{len(records)} records saved to: {records[0].__tablename__}')
 
-    def save_single(self, table_name: str, record: Union[SatelliteImageMetadata, WeatherHourly]):
-        session = self._create_session(table_name)
+    def save_single(self, db_name: str, record: Union[SatelliteImageMetadata, WeatherHourly]):
+        session = self._create_session(db_name)
 
         session.add(record)
         session.commit()
-        self.logger.info(f'Record saved to: {table_name}')
+        self.logger.info(f'Record saved to: {record.__tablename__}')
