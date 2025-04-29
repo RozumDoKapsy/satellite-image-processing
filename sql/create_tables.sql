dd860ea26e61f86b990fd3900af1302ad188da7e
RozumDoKapsy <sulda.str@gmail.com>
2025-04-29 18:24:25 +0200

    feat: create postgresql new table - weather_hourly

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -10,3 +10,17 @@ CREATE TABLE IF NOT EXISTS satellite_images_metadata (
     image_path VARCHAR NOT NULL,
     extraction_date DATE NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS weather_hourly (
+   id SERIAL PRIMARY KEY,
+   location_name VARCHAR NOT NULL,
+   latitude FLOAT NOT NULL,
+   longitude FLOAT NOT NULL,
+   timestamp TIMESTAMP NOT NULL,
+   temperature_2m FLOAT,
+   precipitation FLOAT,
+   rain FLOAT,
+   soil_temperature_0cm FLOAT,
+   soil_moisture_0_to_1cm FLOAT,
+   extraction_date DATE NOT NULL
+);
