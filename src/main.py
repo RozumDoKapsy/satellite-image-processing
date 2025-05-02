fc7ce73789532d2a7060d056cd838d87c17c0d75
RozumDoKapsy <sulda.str@gmail.com>
2025-05-02 18:15:52 +0200

    feat: add processing OpenMeteoPipeline to main.py and added new parameters to CONFIG

--- src/main.py
+++ src/main.py
@@ -1,4 +1,8 @@
 from src.extractors.sentinel_hub import SentinelDataPipeline
+from src.extractors.open_meteo import OpenMeteoPipeline
+
+from src.utils.log_utils import setup_logger
+
 
 CONFIG = {
     'location': {
@@ -10,13 +14,25 @@ CONFIG = {
             'max_lat': 50.0859
         }
     },
-    'sentinel_type': 'sentinel-2-l2a'
+    'sentinel_type': 'sentinel-2-l2a',
+    'weather_frequency': 'hourly',
+    'weather_variables': [
+        'temperature_2m',
+        'precipitation',
+        'rain',
+        'soil_temperature_0cm',
+        'soil_moisture_0_to_1cm'
+    ]
 }
 
 
 def main():
+    setup_logger('extraction')
     sdp = SentinelDataPipeline(CONFIG)
-    sdp.run(n_days=5)
+    sdp.run(n_days=1)
+
+    omp = OpenMeteoPipeline(CONFIG)
+    omp.run(n_days=5)
 
 
 if __name__ == '__main__':
