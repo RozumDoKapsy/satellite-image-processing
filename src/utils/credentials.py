7000c2bd8acd6e1b4bd0623ca9fcbf33c80c9b21
RozumDoKapsy <sulda.str@gmail.com>
2025-10-11 17:09:54 +0200

    feat: add handling for multiple .env file, especially for running locally

--- src/utils/credentials.py
+++ src/utils/credentials.py
@@ -7,29 +7,40 @@ from typing import Union, Dict, Optional
 
 class CredentialManager:
     def __init__(self, dotenv_path: Optional[Union[str, Path]] = None):
-        dotenv_path = dotenv_path or Path(__file__).resolve().parents[2] / '.env'
-        if dotenv_path.exists():
-            load_dotenv(dotenv_path=dotenv_path)
+        root_dir = Path(__file__).resolve().parents[2]
+
+        env_files = [
+            root_dir / '.env',
+            root_dir / '.env.local',
+            root_dir / '.env.dev'
+        ]
+
+        if dotenv_path:
+            env_files.insert(0, Path(dotenv_path))
+
+        for file in env_files:
+            if file.exists():
+                load_dotenv(dotenv_path=file, override=True)
 
     @staticmethod
     def get_sentinelhub_credentials() -> Dict[str, str]:
         return {
-            "client_id": os.getenv("SENTINELHUB_CLIENT_ID"),
-            "client_secret": os.getenv("SENTINELHUB_CLIENT_SECRET")
+            'client_id': os.getenv('SENTINELHUB_CLIENT_ID'),
+            'client_secret': os.getenv('SENTINELHUB_CLIENT_SECRET')
         }
 
     @staticmethod
     def get_minio_credentials():
         return {
-            "access_key": os.getenv("MINIO_ACCESS_KEY"),
-            "secret_key": os.getenv("MINIO_SECRET_KEY"),
-            "endpoint": os.getenv("MINIO_ENDPOINT")
+            'access_key': os.getenv('MINIO_ACCESS_KEY'),
+            'secret_key': os.getenv('MINIO_SECRET_KEY'),
+            'endpoint': os.getenv('MINIO_ENDPOINT')
         }
 
     @staticmethod
     def get_pg_credentials():
         return {
-            "hostname": os.getenv("SATELLITE_POSTGRES_HOSTNAME"),
-            "username": os.getenv("SATELLITE_POSTGRES_TECHNICAL_USER"),
-            "password": os.getenv("SATELLITE_POSTGRES_TECHNICAL_PASSWORD")
+            'hostname': os.getenv('SATELLITE_POSTGRES_HOSTNAME'),
+            'username': os.getenv('SATELLITE_POSTGRES_TECHNICAL_USER'),
+            'password': os.getenv('SATELLITE_POSTGRES_TECHNICAL_PASSWORD')
         }
